ad52f8c9f9b5a6c6ee771193913af7fa1bca0095
JoseK21 <josek21nunez@gmail.com>
2020-05-18 23:03:59 -0600

    Client C Againt F

--- Client.c
+++ Client.c
@@ -1,47 +1,46 @@
 
-// Client side C/C++ program to demonstrate Socket programming
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-#define PORT 1717
+// Client side C/C++ program to demonstrate Socket programming 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Image Name :  \t %s \n", argv[1]);
+#define PORT 1717 
 
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "POS /server/img/";
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
+   
+int main(int argc, char const *argv[]) 
+{ 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
 
     //Get Picture Size
     printf("Getting Picture Size\n");
     FILE *picture;
     picture = fopen(argv[1], "r");
-    /* picture = fopen("/home/josek/Desktop/venado.jpg", "r"); */
     int size;
     fseek(picture, 0, SEEK_END);
     size = ftell(picture);
@@ -54,12 +53,11 @@ int main(int argc, char const *argv[])
     //Send Picture as Byte Array
     printf("Sending Picture as Byte Array\n");
     char send_buffer[size];
-    while (!feof(picture))
-    {
+    while(!feof(picture)) {
         fread(send_buffer, 1, sizeof(send_buffer), picture);
         write(sock, send_buffer, sizeof(send_buffer));
         bzero(send_buffer, sizeof(send_buffer));
     }
 
-    return 0;
-}
+    return 0; 
+} 
